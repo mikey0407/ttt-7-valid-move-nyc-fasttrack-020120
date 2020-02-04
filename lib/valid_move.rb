f24fb03737faeb5f77126d30ebc92ec0f9654fa5
Michael Alfaro <michael.s.alfaro@gmail.com>
2020-02-04 04:27:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,11 @@
  def valid_move? ( board, index)
    def position_taken?(board, index)
-   
+   if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
    
     if index.is_a?(integer) &&
     index.between?(0,8) &&
