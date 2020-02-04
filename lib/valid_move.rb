843b6352103de03c5bd8ec177c8ae169a8cdf31e
Michael Alfaro <michael.s.alfaro@gmail.com>
2020-02-04 04:25:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
  def valid_move? ( board, index)
- def position_taken?(board, index)
- if index.is_a?(integer) &&
-  index.between?(0,8) &&
+   def position_taken?(board, index)
+    if index.is_a?(integer) &&
+    index.between?(0,8) &&
   not position_taken?(board, index)
   true
  else
